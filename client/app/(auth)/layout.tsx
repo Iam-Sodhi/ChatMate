2401230a244fa57b4d31a782f1da6c69d8e3eeb1
Gautam Sodhi <129111379+Iam-Sodhi@users.noreply.github.com>
2023-09-07 00:55:50 +0530

    Delete client directory

--- client/app/(auth)/layout.tsx
+++
@@ -1,8 +0,0 @@
-const AuthLayout=({children}:{children: React.ReactNode})=>{
-    return(
-        <div className="bg-black/75 h-screen flex justify-center items-center">
-            {children}
-        </div>
-    );
-}
-export default AuthLayout;
